f051c23cdf039678d8082fa39841a1824a4b08e5
pynix <pynix.wang@gmail.com>
2017-03-11 20:39:22 +0800

    fix weibo uid type error

--- lib/token_validator/weibo.rb
+++ lib/token_validator/weibo.rb
@@ -2,7 +2,7 @@ module TokenValidator
   class Weibo
     def validate uid, token
       url = "https://api.weibo.com/oauth2/get_token_info"
-      MultiJson.load(Faraday.post(url, access_token: token).body)['uid'] == uid
+      MultiJson.load(Faraday.post(url, access_token: token).body)['uid'].to_s == uid
     end
   end
 
