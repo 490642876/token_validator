700f4803f33dbe24914c6a717b4db416e4532123
lizl <490642876@qq.com>
2020-03-30 17:30:10 +0800

    upcase

--- lib/token_validator/facebook.rb
+++ lib/token_validator/facebook.rb
@@ -1,7 +1,7 @@
 module TokenValidator
   class Facebook
     def validate uid, token
-      url = "https://graph.facebook.com/debug_token?input_token=#{token}&access_token=#{ENV["facebook_app_id"]}|#{ENV["facebook_app_secret"]}"
+      url = "https://graph.facebook.com/debug_token?input_token=#{token}&access_token=#{ENV["FACEBOOK_APP_ID"]}|#{ENV["FACEBOOK_APP_SECRET"]}"
       facebook_response = MultiJson.load(Faraday.get(url).body)
       facebook_response["data"]["is_valid"] && facebook_response["data"]["user_id"].to_s == uid
     end
