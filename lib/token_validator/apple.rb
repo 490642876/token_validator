1c2b00999ae6dcdfb4c7ad89c5732959a4e3c1a3
lizl <490642876@qq.com>
2020-03-30 17:51:07 +0800

    ==

--- lib/token_validator/apple.rb
+++ lib/token_validator/apple.rb
@@ -10,7 +10,7 @@ module TokenValidator
         kid = header_segment["kid"]
         apple_response = Faraday.get(APPLE_PEM_URL).body
         apple_certificate = JSON.parse(apple_response)
-        apple_key = apple_certificate["keys"].find {|h| h["kid"] = kid}
+        apple_key = apple_certificate["keys"].find {|h| h["kid"] == kid}
         apple_key = apple_key.inject({}) {|h, (k, v)| h[k.to_sym] = v; h}
         jwk = JWT::JWK.import(apple_key)
         token_data = JWT.decode(token, jwk.public_key, true, {algorithm: alg})[0]
